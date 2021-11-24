12f6daf1cc710f1aea9a697dcafbff7432b33d11
Aditya Vardhan <56600701+vardhan2000@users.noreply.github.com>
2021-11-24 18:54:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from program import fibonacci
 class TestFibonacci(unittest.TestCase):
     def test_fib1(self):
         result1 = fibonacci(9)
-        self.assertEqual(result1, 34)
+        self.assertEqual(result1, 30)
     def test_fib2(self):
         result2 = fibonacci(0)
         self.assertEqual(result2, 0)
